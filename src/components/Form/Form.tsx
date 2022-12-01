5fccc092fdffbc4923ab1ee9cddf7969fde35d15
Lone1106 <97834596+Lone1106@users.noreply.github.com>
2022-12-01 11:46:35 +0100

    Form styling and basic functionalit

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,33 +1,60 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import classes from "./Form.module.css";
 
 const Form: React.FC = () => {
+    const spendingName = useRef();
+    const amount = useRef();
+    const [radio, setRadio] = useState();
+
+    const getFormData = (e) => {
+        e.preventDefault();
+        // HANDLE FORM DATA
+    };
+
     return (
         <section className={classes.section}>
             <h1>Monthly Spendings</h1>
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={getFormData}>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    id="name"
+                    className={classes.input}
+                    required
+                    ref={spendingName}
+                />
                 <div className={classes.wrapper}>
                     <div>
-                        <label htmlFor="select">Type</label>
-                        <select id="select">
-                            <option value="in">Income</option>
-                            <option value="out">Spending</option>
-                        </select>
-                    </div>
-
-                    <div>
-                        <label htmlFor="amount">Amount</label>
                         <input
                             id="amount"
+                            className={classes.input}
                             type="number"
                             placeholder="Amount"
                             required
+                            ref={amount}
                         />
                     </div>
-                </div>
+                    <div className={classes.radioGroup}>
+                        <label htmlFor="select">In</label>
+                        <input
+                            type="radio"
+                            name="type"
+                            onChange={() => setRadio(0)}
+                        />
+                    </div>
 
-                <button type="Submit">Add</button>
+                    <div className={classes.radioGroup}>
+                        <label htmlFor="select">Out</label>
+                        <input
+                            type="radio"
+                            name="type"
+                            defaultChecked
+                            onChange={() => setRadio(1)}
+                        />
+                    </div>
+                    <button type="Submit">Add</button>
+                </div>
             </form>
         </section>
     );
